53f9145903f2fa75b6eb867d9f1a0e1f85cb419e
Akhil Verma <42695861+AkhilVerma1@users.noreply.github.com>
2024-05-03 22:32:02 +0530

    Initial Commit

---
+++ Disney Characters/Disney_CharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Disney_CharactersApp.swift
+//  Disney Characters
+//
+//  Created by Akhil Verma on 03/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Disney_CharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
