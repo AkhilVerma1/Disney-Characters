a19df7b1ab7305d1f38cfbbe2ce72ab1b81e96a1
Akhil Verma <42695861+AkhilVerma1@users.noreply.github.com>
2024-05-04 10:38:29 +0530

    WIP: Dashboard View added

---
+++ Disney Characters/Dashboard/View/DCDashboardView.swift
@@ -0,0 +1,19 @@
+//
+//  DCDashboardView.swift
+//  Disney Characters
+//
+//  Created by Akhil Verma on 03/05/24.
+//  Copyright © 2024 OCloud Labs. All rights reserved.
+//
+
+import SwiftUI
+
+struct DCDashboardView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    DCDashboardView()
+}
